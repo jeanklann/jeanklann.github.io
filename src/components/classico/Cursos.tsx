8811c2b2ffe537cbb45bf16644fb330d5a994fa2
jeanklann <jeanklann@gmail.com>
2025-06-10 11:34:30 -0300

    ajuste visual

--- src/components/classico/Cursos.tsx
+++ src/components/classico/Cursos.tsx
@@ -12,10 +12,10 @@ const Cursos = (props:CursosProps) => {
     const todosCursos = props.dados.map(curso => curso.nomes.map(nome => getText(props.language, nome))).flat();
 
   return (
-    <div className='cursos col-4 row col3'>
+    <div className='cursos col3'>
               <TituloSecao title={getText(props.language, props.palavras.cursos)} type='secondary' />
               <div className='curso-item'>
-                <div className='nomes col-12'>{todosCursos.join(', ')}</div>
+                <div className='nomes'>{todosCursos.join(', ')}</div>
               </div>
             </div>
   )
